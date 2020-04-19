c65d16a2178ae54ea580e7b5ebb02a1bf8829ebf
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 15:32:43 -0300

    Add project files.

---
+++ NumeroMultiplos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumerosMultiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois valores inteiros A e B na sequência, separados por espaço. Exemplo 20 2");
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            int A = int.Parse(entrada[0]);
+            int B = int.Parse(entrada[1]);
+
+            if ((A > B && A % B == 0) | (A < B && B % A == 0))
+                Console.WriteLine("São múltiplos");
+            else
+                Console.WriteLine("Não São múltiplos");
+        }
+    }
+}
