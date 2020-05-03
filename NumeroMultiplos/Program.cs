c2369b9603d63cfd2af29fd3d16d643004933cfb
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:05:31 -0300

    Adicionando tratamento de excessões

--- NumeroMultiplos/Program.cs
+++ NumeroMultiplos/Program.cs
@@ -6,16 +6,27 @@ namespace NumerosMultiplos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite dois valores inteiros A e B na sequência, separados por espaço. Exemplo 20 2");
-            string[] entrada = Console.ReadLine().Split(' ');
+            try
+            {
+                Console.WriteLine("Digite dois valores inteiros A e B na sequência, separados por espaço. Exemplo 20 2");
+                string[] entrada = Console.ReadLine().Split(' ');
 
-            int A = int.Parse(entrada[0]);
-            int B = int.Parse(entrada[1]);
+                int A = int.Parse(entrada[0]);
+                int B = int.Parse(entrada[1]);
 
-            if ((A > B && A % B == 0) | (A < B && B % A == 0))
-                Console.WriteLine("São múltiplos");
-            else
-                Console.WriteLine("Não São múltiplos");
+                if ((A > B && A % B == 0) | (A < B && B % A == 0))
+                    Console.WriteLine("São múltiplos");
+                else
+                    Console.WriteLine("Não São múltiplos");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
+            }
         }
     }
 }
